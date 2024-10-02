41e3d699c19db13c95a5dd50beee83e047fb0c37
Anup <anuppoudel60@gmail.com>
2024-10-02 19:49:32 +1000

    feat(auth):Implement user auth and create customer user dropdown

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import { ClerkProvider } from "@clerk/nextjs";
 
 export const metadata: Metadata = {
   title: "ScheduLifyer",
@@ -16,20 +17,22 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        {/* Header*/}
-        <Header />
-        <main className="min-h-screen bg-gradient-to-b from-blue-50 to-white">
-          {children}
-        </main>
-        {/* Footer*/}
-        <footer className="bg-blue-100 py-12">
-          <div className="container mx-auto px-4 text-center text-gray-600">
-            <p>Made by Anup</p>
-          </div>
-        </footer>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          {/* Header*/}
+          <Header />
+          <main className="min-h-screen bg-gradient-to-b from-blue-50 to-white">
+            {children}
+          </main>
+          {/* Footer*/}
+          <footer className="bg-blue-100 py-12">
+            <div className="container mx-auto px-4 text-center text-gray-600">
+              <p>Made by Anup</p>
+            </div>
+          </footer>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
