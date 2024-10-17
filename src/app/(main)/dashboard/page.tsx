35e59d781537db0b06dc1a0a566e4d453fcaa3bf
Anup <anuppoudel60@gmail.com>
2024-10-18 09:57:13 +1100

    feat: add routing, improve responsiveness, create 404 page

---
+++ src/app/(main)/dashboard/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Dashboard = () => {
+  return <div>Dahsboard page</div>;
+};
+
+export default Dashboard;
---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+const NotFound = () => {
+  return (
+    <div className="text-4xl font-extrabold w-screen pt-96 grid place-items-center">
+      404 -Page Not Found
+    </div>
+  );
+};
+
+export default NotFound;
