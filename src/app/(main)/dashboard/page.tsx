1ab5a443f94ea1d2f3fe8fca339ab26752569582
Anup <anuppoudel60@gmail.com>
2024-10-25 13:04:17 +1100

    feat(user): add username update with validation
    - Fetch user data with useUser hook
    - Add form validation using react-hook-form + zod
    - Implement username update logic

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -1,7 +1,64 @@
-import React from "react";
+"use client";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { useUser } from "@clerk/nextjs";
+import React, { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { UserNameFormValues, userNameSchema } from "@/lib/validators";
 
 const Dashboard = () => {
-  return <div>Dahsboard page</div>;
+  const [origin, setOrigin] = useState<string>("");
+  const { isLoaded, user } = useUser();
+
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver<UserNameFormValues>(userNameSchema),
+  });
+
+  useEffect(() => {
+    setOrigin(window.location.origin);
+    setValue("username", user?.username);
+  }, [isLoaded]);
+
+  const onSubmit = async () => {};
+  return (
+    <div className="space-y-8">
+      <Card>
+        <CardHeader>
+          <CardTitle>Welcome, {user?.firstName}</CardTitle>
+        </CardHeader>
+        {/* Latest Updates below */}
+      </Card>
+
+      <Card>
+        <CardHeader>
+          <CardTitle>Your unique name</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
+            <div>
+              <div className="flex items-center gap-2">
+                <span>{origin}/</span>
+                <Input {...register("username")} placeholder="Username" />
+              </div>
+              {errors.username && (
+                <p className="text-red-500 text-sm mt-1">
+                  {errors?.username?.message}
+                </p>
+              )}
+            </div>
+            <Button type="submit">Update Username</Button>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
 };
 
 export default Dashboard;
